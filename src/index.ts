d3e9851ef2674e70b1f593eb83f9dfe6dededdac
Bjørn Næss <bna@dips.no>
2019-09-22 08:55:48 +0200

    Oppdatert biblioteket til å exportere fra index.ts som gjør det mye lettere å ta inn referanser

---
+++ src/index.ts
@@ -0,0 +1,45 @@
+
+import {openEHR} from './rm/rm-model';
+import {API} from './api';
+import {CTX} from './ctx';
+import {HTTP} from './http';
+import DvOrdinal = openEHR.RM.QuantityPackage.DvOrdinal;
+import DvText = openEHR.RM.TextPackage.DvText;
+import DvCodedText = openEHR.RM.TextPackage.DvCodedText;
+import DvCodePhrase = openEHR.RM.TextPackage.DvCodePhrase;
+import DvQuantity = openEHR.RM.QuantityPackage.DvQuantity;
+import DvCount = openEHR.RM.QuantityPackage.DvCount;
+import DvDate = openEHR.RM.DateTimePackage.DvDate;
+import DvDateTime = openEHR.RM.DateTimePackage.DvDateTime;
+import DvBoolean = openEHR.RM.BasicPackage.DvBoolean;
+import DvDuration = openEHR.RM.DateTimePackage.DvDuration;
+import DvInterval = openEHR.RM.QuantityPackage.DvInterval;
+import DvEhrUri = openEHR.RM.UriPackage.DvEhrUri;
+import DvUri = openEHR.RM.UriPackage.DvUri;
+import DvParsable = openEHR.RM.EncapsulatedPackage.DvParsable;
+import DvMultimedia = openEHR.RM.EncapsulatedPackage.DvMultimedia;
+import DvTime = openEHR.RM.DateTimePackage.DvTime;
+import DvProportion = openEHR.RM.QuantityPackage.DvProportion;
+
+
+
+export {DvOrdinal as DvOrdinal}
+export {DvText as DvText}
+export {DvCodedText as DvCodedText}
+export {DvCodePhrase as DvCodePhrase}
+export {DvQuantity as DvQuantity}
+export {DvBoolean as DvBoolean}
+export {DvDate as DvDate}
+export {DvDateTime as DvDateTime}
+export {DvCount as DvCount}
+export {DvDuration as DvDuration}
+export {DvEhrUri as DvEhrUri}
+export {DvInterval as DvInterval}
+export {DvMultimedia as DvMultimedia}
+export {DvParsable as DvParsable}
+export {DvProportion as DvProportion}
+export {DvTime as DvTime}
+export {DvUri as DvUri}
+export {API as API}
+export {CTX as CTX}
+export {HTTP as HTTP}
